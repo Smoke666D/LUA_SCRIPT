c4b2cbb4e1b60853503b8bce98d62aa3584328cd
SMOKEHOME\smoke <smoke666>
2022-10-17 22:30:42 +0300

    Начал делать даташит

--- lib/Can.lua
+++ lib/Can.lua
@@ -1,14 +1,40 @@
 EXT_CAN_ID = 0x80000000
 CanInput = {}
 CanInput.__index = CanInput
-function CanInput:new ( addr )
-	local obj = { ADDR = addr, data={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0,[6]=0,[7]=0,[8]=0}}
+function CanInput:new ( addr , timeout, mask,  d1,d2,d3,d4,d5,d6,d7,d8 )
+	local obj = { ADDR = addr, data={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0,[6]=0,[7]=0,[8]=0},TimeOut, MASK =0xFF, DefData ={[1],[2],[3],[4],[5],[6],[7],[8]},timer= 0}
 	setmetatable( obj, self )
+	self.TimeOut = timeout !=nil and timeout or 0
+	self.DefFata[1] = (d1 != nil) and d1 or 0
+	self.DefFata[2] = (d2 != nil) and d2 or 0
+	self.DefFata[3] = (d3 != nil) and d3 or 0
+	self.DefFata[4] = (d4 != nil) and d4 or 0
+	self.DefFata[5] = (d5 != nil) and d5 or 0
+	self.DefFata[1] = (d6 != nil) and d6 or 0
+	self.DefFata[1] = (d7 != nil) and d7 or 0
+	self.DefFata[1] = (d8 != nil) and d8 or 0
+	self.MASK       = (mask !=nil) and mask or 0
         setCanFilter(addr)
 	return obj
 end
 function CanInput:process()
-   GetCanToTable( self.ADDR,self.data)
+   self.timer = self.timer + getDelay()
+   if  GetCanToTable( self.ADDR,self.data) == 0 then
+	if ((self.TimeOut != 0) and (self.timer > self.TimeOut)) then
+	   if  (MASK & 0x01) then  self.data[1] = self.DefData[1]
+	   if  (MASK & 0x02) then  self.data[2] = self.DefData[2]
+	   if  (MASK & 0x04) then  self.data[3] = self.DefData[3]
+	   if  (MASK & 0x08) then  self.data[4] = self.DefData[4]
+	   if  (MASK & 0x10) then  self.data[5] = self.DefData[5]
+	   if  (MASK & 0x20) then  self.data[6] = self.DefData[6]
+	   if  (MASK & 0x40) then  self.data[7] = self.DefData[7]
+	   if  (MASK & 0x80) then  self.data[8] = self.DefData[8]
+           self.timer = 0
+	end	
+   else
+       self.timer = 0
+   end
+
 end
 function CanInput:getBit( nb, nbit)
 	return ((self.data[nb] & (0x01<<(nbit-1))) >0 ) and true or false
@@ -17,7 +43,7 @@ function CanInput:getByte( nb )
 	return self.data[nb]
 end
 function CanInput:getWord( nb )
-	return self.data[nb]<<8 | self.data[nb+1]
+	return (nb < 7) and ( self.data[nb]<<8 | self.data[nb+1]) or 0
 end
 CanOut = {}
 CanOut.__index = CanOut
