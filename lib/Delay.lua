c80c4056db1ee9030d741a619cdb2eda2fe62b46
Igor <smoke-lmt@mail.ru>
2023-03-14 17:46:10 +0300

    Начал писать руководство по системным функциям

--- lib/Delay.lua
+++ lib/Delay.lua
@@ -1,29 +1,34 @@
 Delay = {}
 Delay.__index = Delay
-function Delay:new ( inDelay , neg )
-	local obj  = { counter = 0, launched = false, output = not neg, delay = inDelay , state = neg, rst = true}
+function Delay:new ( inDelay , pulse)
+	local obj  = { counter = 0, launched = false, output = false, delay = inDelay ,  mode = pulse, satate = false}
 	setmetatable( obj, self )
 	return obj
 end
-function Delay:process ( start )
-	if start == true  then
-	   if self.rst == true then
+function Delay:process ( start, disable )
+    
+	if start then
+	  if self.launched == false then
 		self.launched = true
-	        self.counter = 0
-	   end
-	end
-	self.rst = not start
-	if (self.launched == true ) then
-		self.counter = self.counter + getDelay()
-		if ( self.counter < self.delay ) then
-			self.output  = self.state
+		self.counter = 0
+	  end
+	  if self.launched == true then
+	    self.counter = self.counter + getDelay()
+		if self.counter > self.delay then		 			
+		  self.state = true
+		  if self.mode == true then
+		   self.counter = 0
+		  end		  
 		else
-			self.launched = false
-		end
+		 self.state = false
+		end				
+	  end		
 	else
-		self.output   = not self.state
+	   self.launched = false
 	end
-	return
+	self.launched =self.launched and (not disable)
+	self.output = self.state and start and (not disable)
+	return self.output
 end
 function Delay:get ()
 	return self.output
